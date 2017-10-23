929f362c559e29e4dc2d75bc24f18648e671bd32
SamTheBetter <414706590@qq.com>
2017-10-23 20:37:05 +0800

    新增API给创建订单接口00

--- src/main/java/com/sam/dto/OrderDTO.java
+++ src/main/java/com/sam/dto/OrderDTO.java
@@ -1,15 +1,19 @@
 package com.sam.dto;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.sam.dataobject.OrderDetail;
-import com.sam.enums.OrderStatusEnum;
-import com.sam.enums.PayStatusEnum;
+import com.sam.utils.serializer.Date2LongSerializer;
 import lombok.Data;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 
 @Data
+//@JsonSerialize(include = JsonSerialize.Inclusion.NON_NULL)
+//@JsonInclude(JsonInclude.Include.NON_NULL)
 public class OrderDTO {
     private String orderId;
     private String buyerName;
@@ -28,11 +32,13 @@ public class OrderDTO {
     /**
      * 创建时间.
      */
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date createTime;
     /**
      * 更新时间.
      */
+    @JsonSerialize(using = Date2LongSerializer.class)
     private Date updateTime;
 
-    List<OrderDetail> orderDetailList;
+    List<OrderDetail> orderDetailList = new ArrayList<>();
 }
--- src/main/java/com/sam/enums/ResultEnum.java
+++ src/main/java/com/sam/enums/ResultEnum.java
@@ -4,6 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum ResultEnum {
+
+    PARAM_ERROR(1, "参数不正确"),
+    CART_EMPTY(18, "购物车为空"),
     PRODUCT_NOT_EXIT(10, "商品不存在"),
     PRODUCT_STOCK_ERROR(11, "商品库存不正确"),
     ORDER_NOT_EXIT(12, "订单不存在"),
@@ -11,8 +14,7 @@ public enum ResultEnum {
     ORDER_STATUS_ERROR(14, "订单状态不正确"),
     ORDER_UPDATE_FAIL(15, "订单更新失败"),
     ORDER_DETAIL_EMPTY(16, "订单详情为空"),
-    ORDER_PAY_STATUS_ERROR(7, "订单支付状态不正确"),
-    ;
+    ORDER_PAY_STATUS_ERROR(7, "订单支付状态不正确"),;
     private Integer code;
     private String message;
 
