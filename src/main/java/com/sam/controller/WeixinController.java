86b1fc4337bc75b9fea1bee274379413d7a07655
SamTheBetter <414706590@qq.com>
2017-10-23 23:51:42 +0800

    新增API给创建订单接口02

---
+++ src/main/java/com/sam/controller/WeixinController.java
@@ -0,0 +1,28 @@
+package com.sam.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+@RestController
+@RequestMapping("/weixin")
+@Slf4j
+public class WeixinController {
+    @GetMapping("/auth")
+    public void auth(@RequestParam("code") String code) {
+
+        log.info("进入auth方法...");
+        log.info("code={}",code);
+
+        String url = "https://api.weixin.qq.com/sns/oauth2/access_token?appid=wxb9f5023f0ab41e6f&secret=d6066093dc11650e9a1bf33c166656e2&code=" + code + "&grant_type=authorization_code ";
+
+        RestTemplate restTemplate = new RestTemplate();
+
+        String response = restTemplate.getForObject(url, String.class);
+
+        log.info("response={}",response);
+    }
+}
