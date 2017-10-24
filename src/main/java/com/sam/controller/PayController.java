5b213623a9ce2597103e4fdcafb2ba7d54acede9
SamTheBetter <414706590@qq.com>
2017-10-25 00:50:00 +0800

    微信支付API完成待完成

---
+++ src/main/java/com/sam/controller/PayController.java
@@ -0,0 +1,31 @@
+package com.sam.controller;
+
+import com.sam.dto.OrderDTO;
+import com.sam.enums.ResultEnum;
+import com.sam.exception.SellException;
+import com.sam.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+@RequestMapping("/pay")
+public class PayController {
+
+    @Autowired
+    private OrderService orderService;
+
+    @GetMapping("/create")
+    public void create(@RequestParam("orderId") String orderId,
+                       @RequestParam("returnUrl") String returnUrl) {
+        //1.查询订单
+        OrderDTO orderDTO = orderService.findOne(orderId);
+        if (orderDTO == null) {
+            throw new SellException(ResultEnum.ORDER_NOT_EXIT);
+        }
+        //发起支付
+    }
+
+}
---
+++ src/main/java/com/sam/service/PayService.java
@@ -0,0 +1,8 @@
+package com.sam.service;
+
+import com.sam.dto.OrderDTO;
+
+public interface PayService {
+
+    void create(OrderDTO orderDTO);
+}
